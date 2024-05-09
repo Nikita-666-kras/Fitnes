59c083ba8bcaee9642686c7bad3e531715d6a189
Nikita-666-kras <116032886+Nikita-666-kras@users.noreply.github.com>
2024-05-10 01:55:40 +0300

    rabotaet

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 
 
 const app = createApp(App);
-app.mount('#app');
 app.use(router);
+app.mount('#app');
+
 import "bootstrap/dist/js/bootstrap.js"
--- vue-project/src/page/HomePage.vue
+++ vue-project/src/page/HomePage.vue
@@ -1,46 +1,44 @@
-<script setup>
 
 
 
-</script>
 
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Товар 1">
-                <div class="card-body">
-                    <h5 class="card-title">Товар 1</h5>
-                    <p class="card-text">Описание товара 1.</p>
-                    <a href="#" class="btn btn-primary">Купить</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="#" class="card-img-top" alt="Товар 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Тренер</h5>
+                        <p class="card-text">Описание Тренера 1.</p>
+                        <a href="#" class="btn btn-primary">Купить</a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-4">
-            <div class="card">
-                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Товар 2">
-                <div class="card-body">
-                    <h5 class="card-title">Товар 2</h5>
-                    <p class="card-text">Описание товара 2.</p>
-                    <a href="#" class="btn btn-primary">Купить</a>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="#" class="card-img-top" alt="Товар 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Тренер</h5>
+                        <p class="card-text">Описание Тренера 2.</p>
+                        <a href="#" class="btn btn-primary">Купить</a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-4">
-            <div class="card">
-                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Товар 3">
-                <div class="card-body">
-                    <h5 class="card-title">Товар 3</h5>
-                    <p class="card-text">Описание товара 3.</p>
-                    <a href="#" class="btn btn-primary">Купить</a>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="#" class="card-img-top" alt="Товар 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Тренер</h5>
+                        <p class="card-text">Описание Тренера 3.</p>
+                        <a href="#" class="btn btn-primary">Купить</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
   
 </template>
 
